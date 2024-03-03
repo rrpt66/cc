3db6bde17c344ec1ebe8a02de5a60e6de972cac6
rrpt66 <158460098+rrpt66@users.noreply.github.com>
2024-03-03 19:59:46 +0700

    Update 11.bat

--- 11.bat
+++ 11.bat
@@ -12,4 +12,15 @@ goto :menu
 goto check
 
 :menu
-c:\adw.exe /scan /clean
+cls
+echo scan................
+c:\adw.exe /eula /scan /noreboot
+cls
+echo clearing................
+c:\adw.exe /eula /clean /noreboot
+cls
+C:\AdwCleaner\Logs\AdwCleaner[S00].txt
+TIMEOUT 3 /nobreak > nul
+echo -Delete program scan virus....
+TIMEOUT 3 /nobreak > nul
+c:\adw.exe /uninstall  & del /q %temp%\adw.exe
